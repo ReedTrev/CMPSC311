2a53f83af0675530b10c4c41031d852437274c47
aliiip <alisonpastore@icloud.com>
2016-02-08 22:48:51 -0500

    Update wordC.sh

--- wordC.sh
+++ wordC.sh
@@ -1,1 +1,4 @@
-
+tr -cd '[a-zA-Z0-9" ""\n"]' < PangurBan.txt >  PangurBanEdit.txt
+tr "\n" " " < PangurBanEdit.txt > PangurBanEdit2.txt
+tr " " "\n" < PangurBanEdit2.txt > PangurBanEdit.txt
+sort -f  PangurBanEdit.txt

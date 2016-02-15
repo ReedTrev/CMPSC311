cce22c9a502f92bc7e2f2ef37fd3f8ad1c07d038
ReedTrev <reedtrev@gmail.com>
2016-02-15 16:55:31 -0500

    Update wordC.c

--- wordC.c
+++ wordC.c
@@ -10,10 +10,7 @@ struct word_t{
 	struct word_t *next;
 };
 
-/*void clearCharArray(char[] charr, int length){
-	
-	
-};*/
+struct word_t* head = NULL;
 
 void cleanWord(char str[]){
 	int len = strlen(str);
@@ -45,18 +42,19 @@ void cleanWord(char str[]){
 //returns <0 if str1 < str2
 //   ''   >0 if str1 > str2
 //   ''   =0 if str1 = str2
-void listInsert(char str[], struct word_t **head){
+void listInsert(char str[]){
 	//struct word_t *iter = (struct word_t *)malloc(sizeof(struct word_t*));
-	struct word_t *iter = *head;
+	struct word_t *iter = head;
 	struct word_t *new = (struct word_t *)malloc(sizeof(struct word_t*));
+
 	new->word = str;
 	new->count = 1;
 	new->next = NULL;
 
 	printf("The word to be inserted is: %s\n", str);
-	if(*head==NULL){
+	if(iter==NULL){
 		//new->next = *head;
-		*head = new;
+		head = new;
 		printf("Head is NULL\tHead word: %s\n", new->word);		
 	}
 	else{
@@ -66,10 +64,10 @@ void listInsert(char str[], struct word_t **head){
 		while(iter != NULL){
 			printf("\t\tEntered Insert while loop\n");
 			//If we're at head, and new word should come before:
-			if((iter==*head) && (strcmp(str, iter->word)<0)){
+			if((iter==head) && (strcmp(str, iter->word)<0)){
 				printf("\tNew word is new head\n");
-				new->next = *head;
-				new = *head;	
+				new->next = head;
+				head = new;	
 			}
 			//If new word should come after:
 			else if((strcmp(str, iter->word)>0)){
@@ -89,12 +87,13 @@ void listInsert(char str[], struct word_t **head){
 				printf("\tNew word at end of list\n");
 				iter->next = new;
 			}
-			iter++;
+			iter = iter->next;
 		}
 	}
 	
 };
 
+//NOT FIXED YET
 void writeOut(struct word_t **head, FILE *OUT){
 	struct	word_t *iter = (struct word_t *)malloc(sizeof(struct word_t*));
 	fprintf(OUT, "Test123\n");
@@ -120,32 +119,37 @@ int main(int argc, char** argv){
 
 	int count = 1;
 	int cnt = 0;	
-	char str[100];
+	char *str;
 	char word;
 	struct word_t *curr;
 	struct word_t *head;
-	/*struct word_t head = {
+	struct word_t  list= {
 		.word = NULL,
 		.count = 0,
 		.next = NULL
-	};*/
+	};
 	
-	curr = NULL;
+	curr = &list;
 	head = NULL;
 	while(!feof(INFILE)){
+		curr = (struct word_t *)malloc(sizeof(struct word_t*));
 		printf("Entered while loop\n");
+		//count = strlen(str);//get length of word
+		str = (char*)malloc(100); //Initialize space for str
 		count = fscanf(INFILE, "%s", str);//Set str as next word
-		count = strlen(str);//get length of word
-		cleanWord(&str);//clean the word up
-		printf("%s  %d\n",str,count);
-		listInsert(str, &curr);
-		//Save head pointer
+		cleanWord(str);//clean the word up
+		printf("%s  %d\n",str);//Print the cleaned word.
+
+		listInsert(str);//Insert the word into the linked list
+
+		/*//Save head pointer
 		if(cnt == 0){
 			cnt = 1;
 			head = curr;
 			printf("\nHead set to curr\n");
-		}
-		//printf("Word at curr: %s\n", curr->word);
+		};
+		//printf("Word at curr: %s\n", curr->word)
+		curr = head;*/
 	}
 	writeOut(&head, OUTFILE); 
 	return 0;	
